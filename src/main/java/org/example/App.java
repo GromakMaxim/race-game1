38e8ff450dc906d6ea1e5d3d4f986f1c81ce6c1f
MaximGromak <maximgromak@gmail.com>
2022-12-28 18:21:34 +1000

    frame

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,14 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import javax.swing.*;
+import java.awt.*;
+
+public class App {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Java F1");
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        f.setSize(new Dimension(1100, 600));
+        f.setVisible(true);
     }
 }
