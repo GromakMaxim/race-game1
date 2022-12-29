b93665f5339330bf92552531c8e0ca2bd28ad872
MaximGromak <maximgromak@gmail.com>
2022-12-30 01:03:04 +1000

    add enemy 3 and 4

--- src/main/java/org/example/models/Enemy.java
+++ src/main/java/org/example/models/Enemy.java
@@ -9,7 +9,10 @@ import java.util.Random;
 public class Enemy extends AbstractModel {
     private Road road;
 
-    private List<String> imgPaths = List.of("src/main/resources/enemy/enemy_car_1.png", "src/main/resources/enemy/enemy_car_2.png");
+    private List<String> imgPaths = List.of("src/main/resources/enemy/enemy_car_1.png",
+                                            "src/main/resources/enemy/enemy_car_2.png",
+                                            "src/main/resources/enemy/enemy_car_3.png",
+                                            "src/main/resources/enemy/enemy_car_4.png");
 
     public Enemy(int posX, int posY, int speed, Road road) {
         Random random = new Random();
